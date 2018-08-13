d6745f56a8550e2e6cf7bae25b069a61baa287f8
mete0ra <1321671363@qq.com>
2018-08-13 23:45:05 +0800

    update in 2018/8/13

--- 2018_summer_vacation/respond_search/lookup.py
+++ 2018_summer_vacation/respond_search/lookup.py
@@ -14,12 +14,12 @@ index = [['udacity', ['http://udacity.com', 'http://npr.org']],
          ['computing', ['http://acm.org']]]
 
 
-def lookup(index,keyword):
+def lookup(index, keyword):
     for entry in index:
         if entry[0] == keyword:
             return entry[1]
     return []
 
 
-print lookup(index,'udacity')
+print lookup(index, 'udacity')
 # >>> ['http://udacity.com','http://npr.org']
